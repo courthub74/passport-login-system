9fdf5e2f9ed87e78316fe498700da8a444f773b1
Courdevelops <courdevelops@gmail.com>
2022-01-23 14:46:38 -0500

    regfunctional

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const express = require("express")
 //set up the server with app variable calling express as a function
 const app = express()
 
+//require bcrypt
+const bcrypt = require('bcrypt')
+
+//create a variable to store users
+const users = []
+//OR this is where a database comes in
+
 //for assigning ejs
 app.set('view engine', 'ejs')
+
+//access info coming from forms inside of req variable inside of the post method
+app.use(express.urlencoded({ extended: false }))  //req.body.(name field)
 
 //set up routes ("/" - root path)
 app.get("/", (req, res) => {
@@ -17,8 +27,32 @@ app.get("/login", (req, res) => {
     res.render('login.ejs')
 })
 
+//login route (post)
+app.post("/login", (req, res) => {
+
+})
+
+//register route (get)
 app.get("/register", (req, res) => {
     res.render('register.ejs')
+})
+
+//register route (post)
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        //BELOW is if the users are here and not in a database
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
 })
 
 //make servern run pass through port #
