4cfda7ea5e4462a41054f7370cadfca6814dd51d
Courdevelops <courdevelops@gmail.com>
2022-01-23 13:55:56 -0500

    basicpage

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express")
 //set up the server with app variable calling express as a function
 const app = express()
 
+//for assigning ejs
+app.set('view engine', 'ejs')
+
 //set up routes ("/" - root path)
 app.get("/", (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', { name: 'Courdevelops'})
 })
 
 //make servern run pass through port #
