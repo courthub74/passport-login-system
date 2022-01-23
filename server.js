450b63d11abedda697e9cfe21a9c3e0bf239e64b
Courdevelops <courdevelops@gmail.com>
2022-01-23 14:04:27 -0500

    loginregister

--- server.js
+++ server.js
@@ -12,5 +12,14 @@ app.get("/", (req, res) => {
     res.render('index.ejs', { name: 'Courdevelops'})
 })
 
+//login route (get)
+app.get("/login", (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get("/register", (req, res) => {
+    res.render('register.ejs')
+})
+
 //make servern run pass through port #
 app.listen(3000)
